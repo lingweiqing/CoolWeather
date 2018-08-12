cc1a795c76fb97e993bd55c87b45f2ed0335edae
ling_leanrning <ling1471626451@Gmail.com>
2018-08-12 13:25:47 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -9,4 +9,34 @@ import org.litepal.crud.DataSupport;
  * @updataDes
  */
 public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
 }
---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,21 @@
+package com.coolweather.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * @author ling
+ * @des
+ * @updateAuthor
+ * @updataDes
+ */
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address , okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url(address).build();
+
+        client.newCall(request).enqueue(callback);
+    }
+}
