a013db9f990478f4c785a6c97cf038b38d1252f5
ling_leanrning <ling1471626451@Gmail.com>
2018-08-12 10:25:05 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,12 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * @author ling
+ * @des
+ * @updateAuthor
+ * @updataDes
+ */
+public class County extends DataSupport {
+}
